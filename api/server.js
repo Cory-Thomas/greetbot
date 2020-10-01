4c1073d241e1456d7193834bddbb81005291654b
Cory Thomas <cory.a.thomas1992@gmail.com>
2020-10-01 14:03:49 -0400

    added env

--- api/server.js
+++ api/server.js
@@ -3,7 +3,10 @@ const express = require( 'express' );
 const server = express();
 
 server.get( '/', ( req,res ) => {
-    res.status(200).json({ hello: "Hello World" })
-})
+    res.status(200).json({ 
+        hello: "Hello World", 
+        environment: process.env.NODE_ENV 
+    })
+});
 
 module.exports = server;
