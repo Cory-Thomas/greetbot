f2bad3b4f667163cd31f5a2a0d7f29da01977576
Cory Thomas <cory.a.thomas1992@gmail.com>
2020-10-01 14:07:36 -0400

    added heroku port

--- api/server.js
+++ api/server.js
@@ -5,7 +5,8 @@ const server = express();
 server.get( '/', ( req,res ) => {
     res.status(200).json({ 
         hello: "Hello World", 
-        environment: process.env.NODE_ENV 
+        environment: process.env.NODE_ENV,
+        port: `${process.env.PORT}`
     })
 });
 
